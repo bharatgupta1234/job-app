1ad0953e06c087ed16cec150f7bb5cd8d0b24fb8
bharat <bharatgpt4168@gmail.com>
2023-04-16 20:51:09 +0530

    feat: add job apis

---
+++ src/pages/Home/api.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { Job } from "./types";
+
+const baseUrl = "https://643c08da70ea0e66029fddfd.mockapi.io/api/v1/";
+
+const getJobs = async (): Promise<Job[]> => {
+  const response = await axios.get(baseUrl + "jobs");
+  return response.data;
+};
+
+const addJob = async (job: Job): Promise<Job> => {
+  const response = await axios.post(baseUrl + "jobs", {
+    body: job,
+  });
+  return response.data;
+};
+
+const updateJob = async (job: Job): Promise<Job> => {
+  const response = await axios.put(`${baseUrl}jobs/${job.id}`, {
+    body: job,
+  });
+  return response.data;
+};
+
+const deleteJob = async (jobId: number): Promise<boolean> => {
+  const response = await axios.delete(`${baseUrl}jobs/${jobId}`);
+  return response.data;
+};
+
+export { getJobs, addJob, updateJob, deleteJob };
