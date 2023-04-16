246c2178b2f91ecd954224cd1b4025f879948b31
bharat <bharatgpt4168@gmail.com>
2023-04-16 20:52:34 +0530

    feat: update job card ui

--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -1,14 +1,49 @@
 import React from "react";
+// @ts-ignore
 import Netflix from "assets/netflix.svg";
+import { Button } from "components";
+import { Job } from "pages/Home/types";
 
-const JobCard = () => {
+interface Props {
+  jobDetails: Job;
+  onPressApplyNow(): void;
+  onPressExternalApply(): void;
+  containerClassName: string;
+}
+
+const JobCard = ({
+  jobDetails,
+  onPressApplyNow,
+  onPressExternalApply,
+  containerClassName,
+}: Props) => {
   return (
-    <div>
-      <div>
-        <img src={Netflix} />
-      </div>
-      <div>
-        <p></p>
+    <div
+      className={`flex items-start py-16 px-24 border border-grey rounded-lg	w-830 ${containerClassName}`}
+    >
+      <img src={Netflix} alt="company logo" />
+      <div className="ml-8">
+        <div>
+          <p className="text-dark">{jobDetails.title}</p>
+          <p className="text-dark">{`${jobDetails.company} - ${jobDetails.industry}`}</p>
+          <p className="text-placeholder">
+            {`${jobDetails.location} (${jobDetails.remoteType})`}
+          </p>
+        </div>
+        <div className="my-24">
+          <p className="text-dark">Part-Time (9.00 am - 5.00 pm IST)</p>
+          <p className="text-dark mt-8">
+            Experience {`(${jobDetails.minExp} - ${jobDetails.maxExp} years)`}
+          </p>
+          <p className="text-dark mt-8">{`INR (₹) ${jobDetails.minSalary} - ${jobDetails.maxSalary} / Month`}</p>
+          <p className="text-dark mt-8">{`${jobDetails.totalEmployees} employees`}</p>
+        </div>
+        <div className="flex justify-between">
+          <Button name="Apply Now" onPress={() => {}} />
+          <div className="ml-8">
+            <Button name="External Apply" onPress={() => {}} />
+          </div>
+        </div>
       </div>
     </div>
   );
