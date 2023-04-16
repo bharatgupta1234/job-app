12b7191fdc86e193773e7aaa45b2366a12c73214
bharat <bharatgpt4168@gmail.com>
2023-04-16 17:24:48 +0530

    feat: react + typescript + tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html,css}"],
+  theme: {
+    textColor: {
+      dark: "#212121",
+      white: "#FAFAFA",
+      error: "#D86161",
+      placeholder: "#7A7A7A",
+    },
+    borderColor: {
+      grey: "#E6E6E6",
+    },
+    colors: {
+      primary: "#1597E4",
+      white: "#FFFFFF",
+    },
+    spacing: {
+      4: "4px",
+      8: "8px",
+      16: "16px",
+      32: "32px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
