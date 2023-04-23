e685e7d49e395633f58402f0f76a2488cc371d40
bharat <bharatgpt4168@gmail.com>
2023-04-23 12:02:12 +0530

    feat: update types

--- src/pages/Home/types.ts
+++ src/pages/Home/types.ts
@@ -2,14 +2,19 @@ export interface Job {
   id: number;
   title: string;
   company: string;
-  location: string;
-  remoteType: string;
-  minExp: number;
-  maxExp: number;
+  location?: string;
+  remoteType?: string;
+  minExp?: string;
+  maxExp?: string;
   industry: string;
-  minSalary: number;
-  maxSalary: number;
-  totalEmployees: number;
+  minSalary?: string;
+  maxSalary?: string;
+  totalEmployees?: string;
   isQuickApply: boolean;
   isExternalApply: boolean;
+}
+
+export enum CreateJobSteps {
+  Step1 = "Step1",
+  Step2 = "Step2",
 }
